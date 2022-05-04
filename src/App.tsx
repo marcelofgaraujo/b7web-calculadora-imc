863cc685d9e152252322ea08c50f856d7ec9c66d
felipealpino <felipealpino1@gmail.com>
2022-05-03 23:54:15 -0300

    toast implementation test

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,58 @@
-import React, { useState } from 'react';
-import * as C from './appStyles'
-import Card from './components/card'
-import { listStatus } from './components/listStatus'
+import React, { useCallback, useState } from "react";
+import { useToast } from "react-components";
+import { ElementStatus } from "react-components/dist/shared/theme/colors";
+import * as C from "./appStyles";
+import Card from "./components/card";
+import { listStatus } from "./components/listStatus";
 
 function App() {
   const [height, setHeight] = useState<number>();
   const [weight, setWeight] = useState<number>();
   const [IMC, setIMC] = useState<number>(0);
-  const [currentSituation, setCurrentSituation] = useState<string>()
+  const [currentSituation, setCurrentSituation] = useState<string>();
+  const { addToast } = useToast();
 
-  const handleHeight = (e:React.ChangeEvent<HTMLInputElement>) => {
-    setHeight(parseFloat(e.target.value))
-  }
+  const handleHeight = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setHeight(parseFloat(e.target.value));
+  };
 
-  const handleWeight = (e:React.ChangeEvent<HTMLInputElement>) => {
-    setWeight(parseFloat(e.target.value))
-  }
+  const handleWeight = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setWeight(parseFloat(e.target.value));
+  };
+
+  const handleErrorImcCalculate = useCallback(
+    (title: string, subtitle: string, status: ElementStatus = "warning") => {
+      addToast({ title: title, status: status, subtitle: subtitle, duration: 3000 });
+    },
+    [addToast]
+  );
 
   const handleIMC = () => {
-    if (!height || height < 0) return alert('Por favor, informe uma condição válida!')
-    if (!weight || weight < 0) return alert('Por favor, informe uma condição válida!')
+    if (!height || height < 0) {
+      handleErrorImcCalculate("Height", "Height invalido");
+      return;
+    }
+
+    if (!weight || weight < 0) {
+      handleErrorImcCalculate("Weight", "Weight invalido");
+      return;
+    }
+
     // let IMC = weight/(height*height)
-    setIMC(weight/(height*height))
-    console.log(IMC)
-    if (!IMC) return console.error
-    const foundStatus = listStatus.find((list) => IMC > list.bottomLimit && IMC < list.upperLimit)
-    if (!foundStatus) return alert('Por favor, informe uma condição válida!')
-    setCurrentSituation(foundStatus.situation)
-  }
+    setIMC(weight / (height * height));
+    console.log(`IMC`, IMC);
+    if (!IMC) return console.error;
+    const foundStatus = listStatus.find(list => IMC > list.bottomLimit && IMC < list.upperLimit);
+    console.log(`foundStatus`, foundStatus);
+
+    if (!foundStatus) {
+      handleErrorImcCalculate("Status", "Nao foi encontrado nenhum status");
+      return;
+    }
+
+    handleErrorImcCalculate("Sucesso", "IMC calculado com Sucesso!", "success");
+    setCurrentSituation(foundStatus.situation);
+  };
 
   return (
     <C.Body>
@@ -36,16 +61,19 @@ function App() {
         <C.Left>
           <C.Info>
             <h1>Calcule seu IMC.</h1>
-            <p>IMC é a sigla para Índice de Massa Corpórea, parâmetro adotado pela Organização Mundial de Saúde para calcular o peso ideal de cada pessoa.</p>
+            <p>
+              IMC é a sigla para Índice de Massa Corpórea, parâmetro adotado pela Organização Mundial de Saúde para
+              calcular o peso ideal de cada pessoa.
+            </p>
           </C.Info>
           <C.Inputs>
-            <input type="number" onChange={handleHeight} placeholder='Digite sua altura. Ex: 1.5 (em metros)' />
-            <input type="number" onChange={handleWeight} placeholder='Digite seu peso. Ex: 75.3 (em kg)' />
+            <input type="number" onChange={handleHeight} placeholder="Digite sua altura. Ex: 1.5 (em metros)" />
+            <input type="number" onChange={handleWeight} placeholder="Digite seu peso. Ex: 75.3 (em kg)" />
             <button onClick={handleIMC}>Calcular</button>
           </C.Inputs>
         </C.Left>
         <C.Right>
-          {listStatus.map((items, index)=> (
+          {listStatus.map((items, index) => (
             <Card key={index} data={items} isSelected={currentSituation === items.situation} IMC={IMC} />
           ))}
         </C.Right>
