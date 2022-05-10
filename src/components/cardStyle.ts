ed00c42785333066b1fc6e4cf48504a82f1c49f7
marcelofgaraujo <marcelofgaraujo@gmail.com>
2022-05-09 21:32:08 -0300

    ajuste de posição do svg

--- src/components/cardStyle.ts
+++ src/components/cardStyle.ts
@@ -65,7 +65,7 @@ export const Status = styled.div<StatusProps>`
         svg {
             transform: rotate(90deg);
             top: 0;
-            left: 50%;
+            left: auto;
             height: 40px;
             width: 40px;
         }
